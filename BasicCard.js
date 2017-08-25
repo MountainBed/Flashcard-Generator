d4e5734a1226c0e82740dccd18cdb525bcadffe2
Ross Blair <rblair33@gmail.com>
2017-08-25 15:56:15 -0400

    BasicCard constructed created. Still contains test text that will need to be removed in final upload.

---
+++ BasicCard.js
@@ -0,0 +1,23 @@
+var fs = require("fs");
+
+var BasicCard = function (front, back) {
+  this.front = front;
+  this.back = back;
+  this.cardText = "===Basic Flashcard===\n+\n+  Front: " + this.front + "\n+\n+  Back: " + this.back + "\n+\n===End of Flashcard===\n";
+  this.displayCard = function () {
+    console.log(this.cardText);
+  };
+  this.writeCard = function () {
+    fs.appendFile("log.txt", this.cardText, function (err) {
+      if (err) console.log("There was an error: " + err);
+    });
+  };
+};
+
+module.exports = BasicCard;
+
+var testBasic = new BasicCard("What is my name?", "Ross");
+
+testBasic.displayCard();
+
+testBasic.writeCard();
