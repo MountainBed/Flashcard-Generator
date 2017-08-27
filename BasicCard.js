4307bb10a4b94a68b62fd7909531769e68fcfa3f
Ross Blair <rblair33@gmail.com>
2017-08-27 16:55:25 -0400

    Possible final commit with updated README file.

--- BasicCard.js
+++ BasicCard.js
@@ -1,23 +1,22 @@
 var fs = require("fs");
 
-var BasicCard = function (front, back) {
-  this.front = front;
-  this.back = back;
-  this.cardText = "===Basic Flashcard===\n+\n+  Front: " + this.front + "\n+\n+  Back: " + this.back + "\n+\n===End of Flashcard===\n";
-  this.displayCard = function () {
-    console.log(this.cardText);
-  };
-  this.writeCard = function () {
-    fs.appendFile("log.txt", "\n" + this.cardText, function (err) {
-      if (err) console.log("There was an error: " + err);
-    });
-  };
+function BasicCard (front, back) {
+  if (this instanceof BasicCard) {
+    this.front = front;
+    this.back = back;
+    this.cardText = "===Basic Flashcard===\n+\n+  Front: " + this.front + "\n+\n+  Back: " + this.back + "\n+\n===End of Flashcard===\n";
+    this.logText = "Basic Card:@  Front: " + this.front + "@  Back: " + this.back + "@";
+    this.displayCard = function () {
+      console.log(this.cardText);
+    };
+    this.writeCard = function () {
+      fs.appendFile("log.txt", this.logText, function (err) {
+        if (err) console.log("There was an error: " + err);
+      });
+    };
+  } else {
+    return new BasicCard(front, back);
+  }
 };
 
 module.exports = BasicCard;
-
-var testBasic = new BasicCard("What is my name?", "Ross");
-
-testBasic.displayCard();
-
-testBasic.writeCard();
